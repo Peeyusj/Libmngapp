5b8a9baf69555e977c42d6700ab7085b7c23e83e
Peeyusj <piyushrairai124@gmail.com>
2023-04-03 18:38:12 +0530

    backarrow

--- src/components/AdminLogin.jsx
+++ src/components/AdminLogin.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import '../styles/adminlog.css'
+import{useNavigate} from 'react-router-dom'
+import larrow from "../assets/arrow.png"
+
+
 export const AdminLogin = () => {
+  let nevigator=useNavigate()
+
   return (
     <div className='AdminLogin'>
+      <div className='arim' onClick={()=>nevigator("/")}><img src={larrow} alt="" /></div>
+
       <div className="log">
       <div >
         <input type="text" placeholder='username...' /> <br />
--- src/components/UserLogin.jsx
+++ src/components/UserLogin.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import '../styles/adminlog.css'
-
+import{useNavigate} from 'react-router-dom'
+import larrow from "../assets/arrow.png"
 export const UserLogin = () => {
+  let nevigator=useNavigate()
   return (
      <div className='AdminLogin'>
+      <div className='arim' onClick={()=>nevigator("/")}><img src={larrow} alt="" /></div>
     <div className="log">
     <div >
       <input type="text" placeholder='username...' /> <br />
